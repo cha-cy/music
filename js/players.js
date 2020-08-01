29f75d04072a5f4595b5cf8146d9eb0bd934df36
79 <1962257451@qq.com>
2020-08-01 22:47:28 +0800

    Update and rename player.js to players.js

--- js/players.js
+++ js/players.js
@@ -6,7 +6,7 @@
  *************************************************/
 // 播放器功能配置
 var mkPlayer = {
-    api: "api.php", // api地址
+    api: "//cdn.jsdelivr.net/gh/saigaocy/music/api.php", // api地址
     loadcount: 20,  // 搜索结果一次加载多少条
     method: "POST",     // 数据传输方式(POST/GET)
     defaultlist: 3,    // 默认要显示的播放列表编号
