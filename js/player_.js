79cf1853c08a638dfa7856d706fa37ebd9888886
79 <1962257451@qq.com>
2020-08-01 22:58:57 +0800

    Update and rename player.js to player_.js

--- js/player_.js
+++ js/player_.js
@@ -336,11 +336,6 @@ function play(music) {
     music_bar.lock(false);  // 取消进度条锁定
 }
 
-
-// 我的要求并不高，保留这一句版权信息可好？
-// 保留了，你不会损失什么；而保留版权，是对作者最大的尊重。
-console.info('欢迎使用 MKOnlinePlayer!\n当前版本：'+mkPlayer.version+' \n作者：mengkun(https://mkblog.cn)\n歌曲来源于各大音乐平台\nGithub：https://github.com/mengkunsoft/MKOnlineMusicPlayer');
-
 // 音乐进度条拖动回调函数
 function mBcallback(newVal) {
     var newTime = rem.audio[0].duration * newVal;
